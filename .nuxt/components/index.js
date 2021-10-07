029a169d5770ab6c18ba523d4d523ddc746f6ce5
Nat Hsu <natalie.d.hsu@gmail.com>
2021-10-07 15:50:53 -0400

    init install Nuxt

---
+++ .nuxt/components/index.js
@@ -0,0 +1,7 @@
+import { wrapFunctional } from './utils'
+
+export { default as Logo } from '../../components/Logo.vue'
+export { default as VuetifyLogo } from '../../components/VuetifyLogo.vue'
+
+export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/logo" */).then(c => wrapFunctional(c.default || c))
+export const LazyVuetifyLogo = import('../../components/VuetifyLogo.vue' /* webpackChunkName: "components/vuetify-logo" */).then(c => wrapFunctional(c.default || c))
